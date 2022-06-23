22131057b91e71a4eb9a3df885b5231e8b372dcd
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-06-23 20:25:10 +0300

    fix: fix Main Layout

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: isOpen }">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <li>
+        <a href="#" class="waves-effect waves-orange pointer">{{
+          link.title
+        }}</a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data: () => ({
+    links: [
+      { title: "Счет", url: "/", exact: true },
+      { title: "История", url: "/history" },
+      { title: "Планирование", url: "/planning" },
+      { title: "Новая запись", url: "/record" },
+      { title: "Категории", url: "/categories" },
+    ],
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from "vue";
+import { createApp, VueElement } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "materialize-css/dist/js/materialize.min";
+
+
 
 createApp(App).use(store).use(router).mount("#app");
