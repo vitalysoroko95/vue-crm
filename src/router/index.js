9d39eb8d738aa66c9381f5f572fec0171a71ef49
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-06-23 11:09:10 +0300

    add: add layouts and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,59 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: "/",
+    name: "home",
+    meta: { layout: "main" },
+    component: () => import("../views/HomeView"),
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("../views/LoginView"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("../views/RegisterView"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/CategoriesView"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("../views/RecordView"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/ProfileView"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/PlanningView"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/HistoryView"),
+  },
+  {
+    path: "/detail-record",
+    name: "detail-record",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecord"),
   },
 ];
 
